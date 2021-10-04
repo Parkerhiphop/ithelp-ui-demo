1225e56ca168a8dc9c8b4254b0c3efaf5178b4f4
Parker Chang <45290853+Parkerhiphop@users.noreply.github.com>
2021-10-04 15:28:02 +0800

    feat(portal): implement

---
+++ src/components/Portal/Portal.tsx
@@ -0,0 +1,53 @@
+import {
+  FC,
+  ReactNode,
+  useEffect,
+  useState,
+} from 'react';
+import { createPortal } from 'react-dom';
+import { ElementGetter, getElement } from '../../utils/getElement';
+
+export interface PortalProps {
+  /**
+   * The element you want to portal.
+   */
+  children?: ReactNode;
+  /**
+   * The destination where to portal.
+   */
+  container?: ElementGetter;
+  /**
+   * Whether to disable portal.
+   * If true, it will be a normal component.
+   * @default false
+   */
+  disablePortal?: boolean;
+}
+
+const Portal: FC<PortalProps> = (props) => {
+  const {
+    children,
+    container,
+    disablePortal = false,
+  } = props;
+
+  const [portalElement, setPortalElement] = useState(() => (
+    disablePortal
+      ? null
+      : getElement(container) || document.body
+  ));
+
+  useEffect(() => {
+    if (!disablePortal) {
+      setPortalElement(getElement(container) || document.body);
+    }
+  }, [container, disablePortal]);
+
+  if (disablePortal || !portalElement) {
+    return <>{children}</>;
+  }
+
+  return createPortal(children, portalElement);
+};
+
+export default Portal;
